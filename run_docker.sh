fe73d7a110b96200f314f6154a90eb5ae49b17d0
ars3m <arsene@mitsiomotu.com>
2023-03-13 02:35:04 +0000

    feat: initial commit

---
+++ run_docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+TAG=udacity-mlops-project
+LOCAL_PORT=8000
+DOCKER_PORT=80
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag=$TAG .
+# Step 2: 
+# List docker images
+docker image ls
+# Step 3: 
+# Run flask app
+docker run -p ${LOCAL_PORT}:${DOCKER_PORT} $TAG
---
+++ run_kubernetes.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+TAG=udacity-mlops-project
+DOCKERUSER=arsmits
+LOCAL_PORT=8000
+DOCKER_PORT=80
+dockerpath=$DOCKERUSER/$TAG
+# dockerpath=<>
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run $TAG\
+	--image=$dockerpath\
+	--port=DOCKER_PORT --labels app=$TAG
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward $TAG ${LOCAL_PORT}:${DOCKER_PORT}
